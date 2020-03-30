23a5653180e4f3e7a1787c34b834dcc7284016b4
Sheik Muthu <Sheik.Muthu@sisystems.com>
2020-03-30 13:10:17 +0530

    Add project files.

---
+++ FirebaseCrashReport/FirebaseCrashReport/ViewModels/CrashReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirebaseCrashReport.ViewModels
+{
+    public class CrashReportViewModel
+    {
+        public CrashReportViewModel()
+        {
+
+        }
+    }
+}
---
+++ FirebaseCrashReport/FirebaseCrashReport/Views/CrashReportPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using FirebaseCrashReport.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirebaseCrashReport.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CrashReportPage : ContentPage
+    {
+        public CrashReportPage()
+        {
+            InitializeComponent();
+            BindingContext = new CrashReportViewModel();
+        }
+    }
+}
